aa6a25eaaad2742a9a4c73b7055716229a6e0193
Sam Yong <yongmc.sam2014@outlook.com>
2019-02-09 13:09:36 -0500

    database actions

---
+++ scripts/db.js
@@ -0,0 +1,30 @@
+/*
+ * db.js
+ * waimai-915103765
+ *
+ * handle query to database
+ */
+
+function get_loc(username) {
+  const db = firebase.firestore()
+  const ref = db.collection('users').doc(username)
+  return ref.get().then(doc => doc.data().location)
+}
+
+function set_loc(username, location) {
+  const db = firebase.firestore()
+  const ref = db.collection('users').doc(username)
+  ref.update({ location: location })
+}
+
+function user_info(username) {
+  const db = firebase.firestore()
+  const ref = db.collection('users').doc(username)
+  return ref.get().then(doc => doc.data())
+}
+
+function all_biz(username) {
+  const db = firebase.firestore()
+  return db.collection('users').where('user_type', '==', true)
+}
+
